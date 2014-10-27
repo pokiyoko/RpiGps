260edcc53d19a73a521cd7de1336ceba6147a662
Victor <vcamargo.e@gmail.com>
2014-10-27 10:23:52 -0500

    - update!

---
+++ GPS/Stream.py
@@ -0,0 +1,17 @@
+from serial import Serial
+import pynmea2
+
+GPS = Serial(port = '/dev/ttyUSB0', baudrate = 4800)
+GPS.flush()
+
+for i in range(10):
+	inp_data = GPS.readline()
+	msg = pynmea2.parse(inp_data)
+	try:
+		print msg.timestamp
+		print "Longitude: {}".format(msg.longitude)
+		print "Latitude: {}".format(msg.latitude)
+		print "Speed: {}".format(msg.speed)
+	except:
+		pass
+	
