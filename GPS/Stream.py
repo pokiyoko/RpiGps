2f2743ce58e14298cb8319400ad5bfc93c3a3338
Azrrael-exe <vcamargo.e@gmail.com>
2014-10-27 21:43:01 -0500

    - Ready To Test

--- GPS/Stream.py
+++ GPS/Stream.py
@@ -7,11 +7,13 @@ GPS.flush()
 for i in range(10):
 	inp_data = GPS.readline()
 	msg = pynmea2.parse(inp_data)
+	print msg
 	try:
 		print msg.timestamp
 		print "Longitude: {}".format(msg.longitude)
 		print "Latitude: {}".format(msg.latitude)
-		print "Speed: {}".format(msg.speed)
+		print "Speed: {}".format(msg.spd_over_grnd)
+		print msg
 	except:
 		pass
 	
