db97b06b67e42210968439c39687e129d56a42cf
Azrrael <vcamargo.e@gmail.com>
2014-10-29 00:05:35 -0500

    Remove Try!

--- GPS/TestEmail.py
+++ GPS/TestEmail.py
@@ -21,18 +21,16 @@ def sendMail(sender = user , psw = psd, toRecipe = maillist, sub = topic, Info =
 
 	            # Prepare actual message
 	message = """\From: %s\nTo: %s\nSubject: %s\n\n%s""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
-	try:
-		#server = smtplib.SMTP(SERVER) 
-		server = smtplib.SMTP("smtp.gmail.com", 587) #or port 465 doesn't seem to work!
-		server.ehlo()
-		server.starttls()
-		server.login(gmail_user, gmail_pwd)
-		server.sendmail(FROM, TO, message)
-		#server.quit()
-		server.close()
-		print 'successfully sent the mail'
-	except:
-		print "failed to send mail"
+
+	#server = smtplib.SMTP(SERVER) 
+	server = smtplib.SMTP("smtp.gmail.com", 587) #or port 465 doesn't seem to work!
+	server.ehlo()
+	server.starttls()
+	server.login(gmail_user, gmail_pwd)
+	server.sendmail(FROM, TO, message)
+	#server.quit()
+	server.close()
+	print 'successfully sent the mail'
 
 
 sendMail()
